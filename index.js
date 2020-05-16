bcdaee9fe2bbf07b1ad2bed54f15d85413610b9c
Shau Yin Luk <ivan.luk@icloud.com>
2020-05-16 13:49:03 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (record) => {
+  let resulta = record.find( obj  => obj.result === "W")
+  if (!resulta) {
+    return undefined
+  } else {
+    return resulta.year
+  }
+}
