18342e1d29eb2cf6769c361b4f50680918289829
doodleman218 <dcwolff218@gmail.com>
2020-05-18 10:03:56 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// superbowlWin = (array) => {
+//   array.find(function(array){
+//     if (array.result === "W"){
+//       return array.year
+//     } else {
+//       return undefined
+//     }    
+// })
+// }
+
+// const findWin = (array) => array.result === "W"
+
+superbowlWin = (array) => {
+ let result = array.find( (array) => array.result === "W") 
+    if (result === undefined ){
+      return undefined
+    } else {
+      return result.year
+    }    
+}
