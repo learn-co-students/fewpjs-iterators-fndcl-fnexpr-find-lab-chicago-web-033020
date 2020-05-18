bd187786ba8a7a8f5569b59d078b17b5f9059052
jguz17 <johnathanguzman17@gmail.com>
2020-05-18 08:28:18 -0500

    done

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  const results = arr.find(element => element.result === "W");
+  if (results)
+    return results.year;
+  else
+    return undefined;
+};
